ed3f852009ce5b7afcdac9f33d011d21e04ef62a
hamza herbou <hamza95herbou@gmail.com>
2024-04-16 09:04:43 +0100

    Update GEventsManager.cs

--- Assets/Scripts/Game Events Manager/GEventsManager.cs
+++ Assets/Scripts/Game Events Manager/GEventsManager.cs
@@ -10,6 +10,11 @@ public static class GEventsManager {
             eventsDict = new Dictionary<GEvents, Action<GData>>();
     }
 
+    /// <summary>
+    /// Add a listener to a given event.
+    /// </summary>
+    /// <param name="eventName">Event name of type (GEvents).</param>
+    /// <param name="listener">Event callback that is invoked after an event occurs.</param>
     public static void AddListener(GEvents _eventName, Action<GData> _listener) {
         if (eventsDict.TryGetValue(_eventName, out Action <GData> _event)) {
             _event += _listener;
@@ -21,6 +26,11 @@ public static class GEventsManager {
         }
     }
 
+    /// <summary>
+    /// Remove a listener of a given event.
+    /// </summary>
+    /// <param name="eventName">Event name of type (GEvents).</param>
+    /// <param name="listener">Event callback that is invoked after an event occurs.</param>
     public static void RemoveListener(GEvents _eventName, Action<GData> _listener) {
         if (eventsDict.TryGetValue(_eventName, out Action<GData> _event)) {
             _event -= _listener;
@@ -28,6 +38,19 @@ public static class GEventsManager {
         }
     }
 
+    /// <summary>
+    /// Trigger an event without data.
+    /// </summary>
+    /// <param name="eventName">Event name of type (GEvents).</param>
+    public static void Invoke(GEvents _eventName) {
+        Invoke(_eventName, null);
+    }
+
+    /// <summary>
+    /// Trigger an event with data.
+    /// </summary>
+    /// <param name="eventName">Event name of type (GEvents).</param>
+    /// <param name="data">Data that you want to pass to listeners.</param>
     public static void Invoke(GEvents _eventName, GData data) {
         if (eventsDict.TryGetValue(_eventName, out Action<GData> _event))
             _event.Invoke(data);
@@ -37,13 +60,23 @@ public static class GEventsManager {
 
 
 
-public class GData : EventArgs {
-    private object[] args;
 
+
+public class GData : EventArgs {
+    private readonly object[] args;
+
+    /// <summary>
+    /// Creates an object to hold data that you want to pass to listeners.
+    /// </summary>
     public GData(params object[] _args) {
         args = _args;
     }
 
+    /// <summary>
+    /// Gets the data at a given index.
+    /// </summary>
+    /// <param name="index">The index of the data in the argument's list.</param>
+    /// <returns>Returns data with a given type and index.</returns>
     public T Get<T>(int index) {
         return (T)args[index];
     }
